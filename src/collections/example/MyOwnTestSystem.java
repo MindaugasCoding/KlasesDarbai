2fc1c62e478331effc71ee3e4ea4cb5adb778df7
Min <mindaugas.masaitis@gmail.com>
2018-02-07 19:54:18 +0200

    Pakeistas testo uzdavinys

--- src/collections/example/MyOwnTestSystem.java
+++ src/collections/example/MyOwnTestSystem.java
@@ -1,14 +1,13 @@
 package collections.example;
 
+import org.omg.CORBA.INTERNAL;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 
 public class MyOwnTestSystem {
@@ -16,14 +15,45 @@ public class MyOwnTestSystem {
     public static void main(String[] args) {
         MyOwnTestSystem myOwnTestSystem = new MyOwnTestSystem();
         Map<Integer, Question> questionMap = myOwnTestSystem.readQuestions();
-
+        List<Integer>questions = new ArrayList<>();
+/*
         for (Integer key : questionMap.keySet()) {
             Question q = questionMap.get(key);
             System.out.println(q.getQuestionTex());
             for (String v : q.getVariants()) {
                 System.out.println(v);
             }
+        }*/
+
+        Scanner sc = new Scanner(System.in);
+        int correectAnswerCount = 0;
+        for (int i = 0; i < 10; i++) {
+            int numb;
+            while (true){
+                Random random = new Random();
+                numb = random.nextInt(questionMap.size()) +1;
+                if(!questions.contains(numb)){
+                    break;
+                }
+            }
+            questions.add(numb);
+            /*Random random = new Random();//naudojama generuoti rando skaicius
+            int numb = random.nextInt(questionMap.size()) + 1; //grazina skaiciu random nuo 1 iki map ilgio
+            if(questions.contains(numb)){
+
+            }*/
+            Question question = questionMap.get(numb);
+            System.out.println(question.getQuestionTex());
+            for (String variant : question.getVariants()) {
+                System.out.println(variant);
+            }
+            System.out.println("Įveskite savo atsakymą >>");
+            int userAnswer = sc.nextInt();
+            if (userAnswer == question.getAnswer()) {
+                correectAnswerCount++;
+            }
         }
+        System.out.println("Jūsų testo balas yra >>>>>" + correectAnswerCount + "<<<<<<");
     }
 
     private Map<Integer, Question> readQuestions() {
@@ -41,7 +71,6 @@ public class MyOwnTestSystem {
                 Question question = new Question(items[0], myItems, answer); //sudedam items i objekta
                 questionMap.put(key++, question);
             }
-
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
